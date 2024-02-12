d21ef0a3aa622467169934db63463abd74b02b6a
devRyan <ryan.kinuthia@student.moringaschool.com>
2024-02-12 06:30:25 +0300

    gif

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -4,28 +4,33 @@ import GifSearch from './GifSearch';
 
 const GifListContainer = () => {
   const [gifs, setGifs] = useState([]);
-  
-  
+  const [error, setError] = useState(null); 
   const handleSearch = (query) => {
-    
-    fetch(`https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g`)
-      .then(response => response.json())
-      .then(data => {
-        
-        setGifs(data.data.slice(0, 3));
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
       })
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => {
+        setGifs(data.data.slice(0, 3));
+        setError(null); 
+      })
+      .catch(error => {
+        setError('Error fetching data. Please try again.'); 
+        console.error('Error fetching data:', error);
+      });
   };
 
   return (
     <div>
-    
       <GifSearch onSearch={handleSearch} />
-
-      
+      {error && <p>{error}</p>} 
       <GifList gifs={gifs} />
     </div>
   );
 };
 
 export default GifListContainer;
+
